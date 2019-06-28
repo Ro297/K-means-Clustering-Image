0f409076c2c566c169532b65b0ca770de95ec4cb
Ro297 <rohan.poddar_ug19@ashoka.edu.in>
2019-06-28 13:50:25 +0530

    first commit

---
+++ image_manipulation.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import cv2
+
+img = 'NL.jpg'
+image = cv2.imread(img)
+img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+gray = cv2.GaussianBlur(image, (7, 7), 0)
+edged = cv2.Canny(gray, 10,20)
+edged = cv2.dilate(edged, None, iterations=1)
+edged = cv2.erode(edged, None, iterations=1)
+
+img1 = img[:, :, 0]
+
+fig = plt.figure()
+
+a = fig.add_subplot(2, 2, 1)
+plt.axis("off")
+imgplot = plt.imshow(img)
+a.set_title('original')
+
+a = fig.add_subplot(2, 2, 2)
+plt.axis("off")
+imgplot = plt.imshow(img1)
+a.set_title('viridis')
+
+a = fig.add_subplot(2, 2, 3)
+plt.axis("off")
+imgplot = plt.imshow(edged)
+a.set_title('edged')
+
+a = fig.add_subplot(2, 2, 4)
+plt.axis("off")
+imgplot = plt.imshow(img1, cmap="hot")
+a.set_title('hot')
+
+plt.show()
---
+++ k-means.py
@@ -0,0 +1,36 @@
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+from utils import *
+
+img = 'NL.jpg'
+clusters = 6
+
+# load the image and convert it from BGR to RGB so that
+# we can dispaly it with matplotlib
+image = cv2.imread(img)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+ 
+# show our image
+plt.figure()
+plt.axis("off")
+plt.imshow(image)
+
+# reshape the image to be a list of pixels
+image = image.reshape((image.shape[0] * image.shape[1], 3))
+
+# cluster the pixel intensities
+clt = KMeans(n_clusters = clusters)
+clt.fit(image)
+
+# build a histogram of clusters and then create a figure
+# representing the number of pixels labeled to each color
+hist = centroid_histogram(clt)
+bar = plot_colors(hist, clt.cluster_centers_)
+ 
+# show our color bart
+plt.figure()
+plt.axis("off")
+plt.imshow(bar)
+plt.show()
